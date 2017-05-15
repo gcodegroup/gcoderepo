56288db4acd8a58e7fbcae59af4743995362e1d4
Administrator <Administrator@Liyang-pc>
2017-05-15 22:14:23 +0100

    ffff

--- gcode/src/main/java/uk/ac/bangor/gcode/RunningParameters.java
+++ gcode/src/main/java/uk/ac/bangor/gcode/RunningParameters.java
@@ -2,7 +2,7 @@ package uk.ac.bangor.gcode;
 
 public class RunningParameters {
     
-    private static final String DEFAULT_INPUT_FILE_PATH = "C:\\Temp\\Test.gcode";
+    private static final     String DEFAULT_INPUT_FILE_PATH = "C:\\Temp\\Test.gcode";
     private static final String DEFAULT_OUTPUT_FILE_PATH = "C:\\Temp\\output.gout";
     private static final double DEFAULT_SPEED = 0.1;
     private static final int DEFAULT_START_DELAY_TIME = 300;
